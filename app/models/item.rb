5214c028e4e791f814ef2c54f10d444a5e91ba4a
DJ-th <85214662+DJ-th@users.noreply.github.com>
2021-07-01 11:52:15 +0900

    first commit2

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,7 +24,7 @@ class Item < ApplicationRecord
       validates :scheduled_delivery_id
     end
       with_options format: { with: /\A[0-9]+\z/ } do
-        validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
+        validates :price, numericality: { greater_than: 299, less_than: 10000000 } 
       end
     end
  end
