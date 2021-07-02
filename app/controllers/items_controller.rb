1a8aa3c41c0e6d8f49078717244cf9a37b674ef0
DJ-th <85214662+DJ-th@users.noreply.github.com>
2021-07-02 11:46:25 +0900

    first commit1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,24 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-
+  def edit
+    @item = Item.find(params[:id])
+    #if user_signed_in?
+       #if current_user.id == @item.user_id
+      #redirect_to root_path
+    #else
+      #render :edit
+    #end
+  #end
+  end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
   def create
     @item = Item.new(item_params)
       if @item.save
